7d700094cb5a0013ddc64ffb988842b77fdaecf0
darshangawade <darshangawade1808@gmail.com>
2022-03-15 16:06:20 +0530

    Prediction using random forest done

--- random_forest_model.R
+++ random_forest_model.R
@@ -15,30 +15,27 @@ train <- df[splitting==1,]
 test <- df[splitting==2,]
 
 
-rf_fit <- randomForest(Chance_of_Admit ~ ., data=df, ntree=1000,
-                       keep.forest=FALSE, importance=TRUE)
-`OOB Sample error` = rf_fit
+rf_fit <- randomForest(Chance_of_Admit ~ ., data=train)
 
 # predicting the chance of admit on the data present in test dadaset
 y_pred = predict(rf_fit,test)
 # creating a new column in test dataset name as `prediction` and inserting y_pred into it.
 test$prediction = y_pred
 
-print(rf_fit)
-plot(`OOB Sample error`)
-summary(rf_fit)
+# input form user
+GRE_Score = c(322)
+TOEFL_Score = c(110)
+University_Rating = c(3)
+SOP = c(3.5)
+LOR = c(2.5)
+CGPA = c(8.67)
+Research = c(1)
 
-ImpData <- as.data.frame(importance(rf.fit))
-ImpData$Var_Name <- row.names(ImpData)
+# creating a dataframe on custom input from user
+datas = data.frame(GRE_Score,TOEFL_Score,University_Rating,SOP,LOR,CGPA,Research)
 
-ggplot(ImpData, aes(x=Var_Name, y=`%IncMSE`)) +
-  geom_segment( aes(x=Var_Name, xend=Var_Name, y=0, yend=`%IncMSE`), color="skyblue") +
-  geom_point(aes(size = IncNodePurity), color="blue", alpha=0.6) +
-  theme_light() +
-  coord_flip() +
-  theme(
-    legend.position="bottom",
-    panel.grid.major.y = element_blank(),
-    panel.border = element_blank(),
-    axis.ticks.y = element_blank()
-  )
+# prediction on custom input
+predict(rf_fit,datas)
+
+res = as.numeric(predict(rf_fit,datas)[1])
+print(paste('Your chance of addmission is',res*100,'%'))
